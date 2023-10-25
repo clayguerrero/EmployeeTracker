4592f965580065f7a8cc364247de1a333a9d119e
clayg <claytondguerrero@gmail.com>
2023-10-25 14:47:39 -0500

    set up condidtional statements

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ const inquirer = require("inquirer");
 const mysql2 = require("mysql2");
 
 function init() {
-  inquirer.createPromptModule({
-    type: "list",
-    name: "starter",
-    messages: "What would you like to do?",
-    choices: [
-      "View All departments",
-      "Add Employees",
-      "Update Employee",
-      "View All Roles",
-      "Add Role",
-      "View All Departments",
-      "Add Department",
-      "View All Employees",
-      "Quit",
-    ],
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      name: "starter",
+      messages: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "Add Employees",
+        "Update Employee",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "View All Employees",
+        "Quit",
+      ],
+    })
+    .then((res) => {
+        if (res.starter === 'View All Departments') {
+            console.log(`You Chose`, res.starter);
+        }
+        else console.log('Error LUL')
+    });
 }
+
+init();
