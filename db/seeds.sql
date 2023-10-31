da449ad4a93ee8768a50bf1d296b182705e7f58d
clayg <claytondguerrero@gmail.com>
2023-10-31 16:48:42 -0500

    bug fixes

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,10 +10,14 @@ INSERT INTO role (title, salary, department_id)
 VALUES
 ('Cashier', 18000, 1),
 ('Shopper', 22000, 2),
-('Manager', 80000, 3);
+('Manager', 80000, 3),
+('Curbie', 15000, 2),
+('Bagger', 15000, 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Christian', 'C', 1, null),
-('Clay', 'G', 2, null),
-('Chuck', 'R', 3, 3);
+('Carlos', 'R', 1, NULL),
+('Ashley', 'B', 2, NULL),
+('Christian', 'C', 1, 1),
+('Clay', 'G', 2, 2),
+('Chuck', 'R', 3, NULL);
--- server.js
+++ server.js
@@ -61,10 +61,63 @@ function viewEmployees() {
   });
 }
 function addEmployees() {
-  const query = "SELECT id, title, FROM role";
-  // db.query(query, (req, res) => {
-  //   console.log
-  // })
+  db.query("SELECT id, title FROM role", (req, result) => {
+   const findRoles = result.map(({ id, title }) => ({
+     name: title,
+     value: id,
+   }));
+
+   db.query(
+     'SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee',
+     (req, result) => {
+
+       const managers = result.map(({ id, name }) => ({
+         name,
+         value: id,
+       }));
+
+       inquirer
+         .prompt([
+           {
+             type: "input",
+             name: "first_name",
+             message: "What is the employee's first name",
+           },
+           {
+             type: "input",
+             name: "last_name",
+             message: "What is the employee's last name?",
+           },
+           {
+             type: "list",
+             name: "role",
+             message: "What is the role of the employee?",
+             choices: findRoles,
+           },
+           {
+             type: "list",
+             name: "manager",
+             message: "Who is their manager",
+             choices: [{ name: "Is a Manager", value: null }, ...managers],
+           },
+         ])
+         .then((data) => {
+           const query =
+             "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+           const values = [
+             data.first_name,
+             data.last_name,
+             data.role,
+             data.manager,
+           ];
+           db.query(query, values, (res) => {
+             console.log("New employee added");
+             init();
+           });
+         })
+     }
+   );
+ });
 }
 function updateEmplyees(res) {
   console.log(`You Chose`, res.starter);
@@ -110,7 +163,7 @@ function addRoles() {
             salary: data.salary,
             department_id: department,
           },
-          console.log(`New role has been added.`)
+          console.log(`New role has been added!`)
         );
         init();
       });
@@ -123,6 +176,18 @@ function viewDepartments() {
   });
   init();
 }
-function addDepartments(res) {
-  console.log(`You Chose`, res.starter);
+function addDepartments() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of the new department?'
+    }
+  ])
+    .then((res) => {
+      const query = `INSERT INTO department (name) VALUES ("${res.name}")`;
+      db.query(query, res)
+      console.log(`New department added!`)
+      init()
+  })
 }
